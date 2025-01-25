ff5645b06d53f667938b1a39af884c82dd6b227c
Oliver <oliver.robb@outlook.com.au>
2025-01-25 21:46:35 +1100

    add randomisation to packet class byte arrays

--- src/sim/packet_pkg.sv
+++ src/sim/packet_pkg.sv
@@ -1,11 +1,38 @@
 import payload_aligner_pkg::*;
 
 package packet_pkg;
-    class packet_randomiser_ct;
-        logic [payload_aligner_pkg::header_a_width_bits - 1:0] header_A;
-        logic [payload_aligner_pkg::header_b_width_bits - 1:0] header_B;
-        logic [payload_aligner_pkg::header_c_width_bits - 1:0] header_C;
-        logic payload[];
+    class packet_ct;
+        byte header_a [payload_aligner_pkg::header_a_width_bytes - 1:0];
+        byte header_b [payload_aligner_pkg::header_b_width_bytes - 1:0];
+        byte header_c [payload_aligner_pkg::header_c_width_bytes - 1:0];
+        byte payload[];
+
+        byte packet[];
+
+        // SystemVerilog rand and randomise() functionallity isn't available without full license.
+        function void randomise(int payload_length = $urandom_range(1, 100));
+            int total_packet_length = payload_aligner_pkg::header_a_width_bytes + payload_aligner_pkg::header_b_width_bytes + payload_aligner_pkg::header_c_width_bytes + payload_length;
+            packet = new[total_packet_length];
+
+            // Randomise each packet field individually before packing into a packet for easier comparison with DUT
+            foreach (header_a[i]) begin
+                header_a[i] = $urandom_range(0, 8'hFF);
+            end
+            foreach (header_a[i]) begin
+                header_b[i] = $urandom_range(0, 8'hFF);
+            end
+            foreach (header_a[i]) begin
+                header_c[i] = $urandom_range(0, 8'hFF);
+            end
+
+            payload = new[payload_length];
+            foreach(payload[i]) begin
+                payload[i] = $urandom_range(0, 8'hFF);
+            end
+            
+            packet = {header_a, header_b, header_c, payload};
+        endfunction
+
     endclass
 
 endpackage
